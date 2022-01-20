33cc493ff851f743b7007e73f8ddb21599390297
Deepak Agarwal <47721949+dennisagarwal@users.noreply.github.com>
2022-01-20 14:52:19 -0500

    logging in

---
+++ src/Components/Login/Login.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { useDispatch } from "react-redux";
+import { login } from "../../features/user";
+
+function Login() {
+  const dispatch = useDispatch();
+
+  return (
+    <div>
+      <button
+        onClick={() => {
+          dispatch(login({ name: "Pedro", age: 20, email: "pedro@gmail.com" }));
+        }}
+      >
+        Login
+      </button>
+
+    </div>
+  );
+}
+
+export default Login;
